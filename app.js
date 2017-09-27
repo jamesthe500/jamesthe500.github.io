cd9f629a2f44305f19d326a5002508aeddec41f4
jamesthe500 <jamesddaniels@gmail.com>
2017-09-27 13:19:17 -0500

    Updates resume to match latest docx

--- app.js
+++ app.js
@@ -2,30 +2,35 @@
 ////// As this list changes, make certain that the length of A >= B >= C  ///////
 var skillsLists = {
         "colA" : [
-            "HTML5/CSS3",
-            "Bootstrap",
-            "Jade",
+            "HTML5",
+            "CSS3",
             "JavaScript",
-            "C++",
-            "Java",
-            "Git/GitHub"
+            "C#",
+            "jQuery",
+            "Bootstrap"
         ],
         "colB" : [
-            "GruntJS",
-            "MySQL",
-            "MongoDB",
-            "APIs",
-            "Node.js",
-            "D3",
-            "MS Office Suite"
+            "MEAN Stack",
+            "&nbsp;&nbsp;&bull;MongoDB",
+            "&nbsp;&nbsp;&bull;Express.js",
+            "&nbsp;&nbsp;&bull;AngularJS",
+            "&nbsp;&nbsp;&bull;Node.js",
+            "ASP.NET"
         ],
         "colC" : [
-            "AngularJS",
-            "jQuery",
+            "MySQL",
             "JSON",
+            "APIs",
             "AJAX",
-            "Adobe Illustrator",
-            "Adobe Photoshop",
+            "Arduino/C++",
+            "Java"
+        ],
+        "colD" : [
+            "Git",
+            "GitHub",
+            "Illustrator",
+            "Photoshop",
+            "&nbsp;&nbsp;&bull;Scripting JS",
             "Final Cut Pro X"
         ]
     };
@@ -33,12 +38,15 @@ var skillsLists = {
 $(document).ready(function(){
 
     for(i = 0 ; i < skillsLists.colA.length ; i++) {
-        $("#skillList").append("<div class='colA col-xs-5'>" + skillsLists.colA[i] + "</div>");
+        $("#skillList").append("<div class='skill colA col-xs-3'>" + skillsLists.colA[i] + "</div>");
 
         if (i >= skillsLists.colB.length){return;}
-        $("#skillList").append("<div class='colB col-xs-3'>" + skillsLists.colB[i] + "</div>");
+        $("#skillList").append("<div class='skill colB col-xs-3'>" + skillsLists.colB[i] + "</div>");
 
         if (i >= skillsLists.colC.length){return;}
-        $("#skillList").append("<div class='colC col-xs-4'>" + skillsLists.colC[i] + "</div>");
+        $("#skillList").append("<div class='skill colC col-xs-3'>" + skillsLists.colC[i] + "</div>");
+        
+        if (i >= skillsLists.colD.length){return;}
+        $("#skillList").append("<div class='skill colD col-xs-3'>" + skillsLists.colD[i] + "</div>");
     }
 }); //end of doc ready
